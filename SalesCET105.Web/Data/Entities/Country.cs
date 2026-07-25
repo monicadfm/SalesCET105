1399a334929720556a79b757aeffdfb9ffa195a0
terumineko <miamonicamoura@gmail.com>
2026-07-25 21:55:21 +0100

    create database and countries table

---
+++ SalesCET105.Web/Data/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesCET105.Web.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Pais")]
+        [MaxLength(50, ErrorMessage = "O campo {0} dever ter no maximo {1} caracters!")]
+        [Required(ErrorMessage ="O campo {0} e obrigatorio!")]
+        public string Name { get; set; }
+    }
+}
